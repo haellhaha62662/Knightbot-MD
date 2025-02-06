c4b6b7cb73dc2f1f43a1be91d6d020bd8954981f
haellhaha62662 <ranotricks@gmail.com>
2025-02-07 00:04:46 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'shahmeer ', // 
   ownerNumber: '923232322834', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
